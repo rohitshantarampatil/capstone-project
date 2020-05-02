a86cc24bc07b8167d95f3ac8a6bb0e9cd7ec3316
rohitshantarampatil <rohit.patil@iitgn.ac.in>
2020-05-02 16:36:18 +0530

    commit 2

--- covid19monitor/client/main.js
+++ covid19monitor/client/main.js
@@ -6,8 +6,50 @@ import './main.html';
 Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
   this.counter = new ReactiveVar(0);
+      function getdata(){  
+        return $.getJSON( "https://api.apify.com/v2/key-value-stores/tVaYRsPHLjNdNBu7S/records/LATEST?disableRedirect=true", function( data ) {
+          var items = [];
+          $.each( data, function( key, val ) {
+            items.push( [key,val] );
+          });
+          return items
+        });
+    }
+
+    data = getdata();
+    // console.log(data);
+
 });
 
+Template.maincard.onCreated(function(){
+  console.log("maincard");
+});
+
+Template.maincard.helpers({
+  country:function(){
+    function getdata(){  
+      return $.getJSON( "https://api.apify.com/v2/key-value-stores/tVaYRsPHLjNdNBu7S/records/LATEST?disableRedirect=true"
+      // , function( data ) {
+      //   // var items = [];
+      //   // $.each( data, function( val ) {
+      //   //   items.push( [val] );
+      //   // });
+      //   // return items
+      //   return data
+      // }
+      );
+  };
+
+  data = getdata();
+  // console.log(typeof(data));
+  console.log(data);
+  a = data.responseJSON
+  console.log(a)
+  // console.log(data.responseJSON);
+  return [{id:5},{id:4}];
+
+  }
+})
 Template.hello.helpers({
   counter() {
     return Template.instance().counter.get();
